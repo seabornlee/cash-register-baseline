f637f2b4a1a29e46174f65a05b24a484f8a19a72
Seaborn Lee <seabornlee.cn@gmail.com>
2018-04-28 09:24:34 +0800

    Revert test

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -21,37 +21,10 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase() {
         //initialize CashRegister and fake Printer
-        MockPrinter mockPrinter = new MockPrinter();
-        CashRegister cashRegister = new CashRegister(mockPrinter);
-        Purchase purchase = new Purchase(new Item[] {
-                new Item("Milk", 5.0)
-        });
 
         cashRegister.process(purchase);
 
         //verify that printer was called
-        mockPrinter.verifyThatPrintWasCalledWith(purchase.asString());
     }
 
-    @Test
-    public void should_print_the_stubbed_purchase() {
-        //initialize CashRegister and fake Printer
-        MockPrinter mockPrinter = new MockPrinter();
-        CashRegister cashRegister = new CashRegister(mockPrinter);
-        StubPurchase purchase = new StubPurchase("test purchase string");
-
-        cashRegister.process(purchase);
-
-        //verify that printer was called
-        mockPrinter.verifyThatPrintWasCalledWith("test purchase string");
-    }
-
-    @Test
-    public void should_mock_with_mockito() throws Exception {
-        when(stubPurchase.asString()).thenReturn("some test text");
-        CashRegister cashRegister = new CashRegister(mockPrinter);
-        cashRegister.process(stubPurchase);
-
-        verify(mockPrinter).print("some test text");
-    }
 }
